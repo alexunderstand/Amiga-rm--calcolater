04d34255d44e6fca28c47beaf23547f0e56a58ed
alexunderstand <salualexander@gmail.com>
2026-08-26 09:30:54 +0200

    Add theoretical 1RM calculator in rm.c
    Implement a program to calculate theoretical 1RM based on weight and repetitions.

---
+++ rm.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    char buffer[40];
+    int vikt;
+    int reps;
+    int rm;
+
+    printf("STYRKELYFT - TEORETISKT 1RM\n");
+    printf("--------------------------\n");
+
+    printf("Ange vikt i kg: ");
+    fgets(buffer, sizeof(buffer), stdin);
+    vikt = atoi(buffer);
+
+    printf("Ange antal repetitioner: ");
+    fgets(buffer, sizeof(buffer), stdin);
+    reps = atoi(buffer);
+
+    rm = vikt + (vikt * reps) / 30;
+
+    printf("\nTeoretiskt 1RM: %d kg\n", rm);
+
+    return 0;
+}
